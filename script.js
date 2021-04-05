39beb81e210da8af5b3546c8a24b189e188c049d
vimarteleto <viniciusmarteleto@gmail.com>
2021-04-05 10:14:36 -0300

    Implementacao de soma

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+// operações
 function operate(f, x, y) {
     return f(x, y)
 }
+
 function add(x, y) {
     return x + y    
 }
@@ -31,3 +33,55 @@ function porcentagem(x) {
 }
 
 console.log(operate(root, 25, 2))
+
+// eventos
+let operacao
+let x // primeiro fator
+let y // segundo fator
+let f // operacao
+let result // resultado
+const tela = document.getElementById('tela')
+const numeros = document.querySelectorAll('.botoes-numero')
+numeros.forEach(numero => {
+    numero.addEventListener('click', () => {
+        if(result) {
+            tela.textContent = numero.textContent
+        }
+        else {
+            tela.textContent += numero.textContent
+        }
+        
+    })
+})
+
+const igual = document.getElementById('igual')
+igual.addEventListener('click', () => {
+    y = Number(tela.textContent)
+    if(f) {
+        result = operate(f, x, y)
+        tela.textContent = result
+    }  
+    console.log(y) 
+    console.log(result)
+})
+
+const soma = document.getElementById('soma')
+soma.addEventListener('click', () => {
+    x = Number(tela.textContent)
+    tela.textContent = ''
+    f = add
+    console.log(x)
+    console.log(f)
+})
+
+const ac = document.getElementById('ac')
+ac.addEventListener('click', () => {
+    x = ''
+    y = ''
+    f = ''
+    result = ''
+    tela.textContent = ''
+})
+
+
+
