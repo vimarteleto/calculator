1f14ebcede56826fe293dc2d7d0619c03a98b3e5
vimarteleto <viniciusmarteleto@gmail.com>
2021-04-27 03:23:56 -0300

    Execuçao dos calculos

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
+// to-do list:
+// suportar expressoes matematicas corretamente
+// suporte para teclado
+// botao backspace
+
+/////////////////////////////
+
 let f
 let x
 let y
+let result
+
 // operações
 function operate(f, x, y) {
     return f(x, y)
@@ -35,39 +44,73 @@ function percentage(x, y) {
 }
 
 // elementos
+// elemento da tela
 const screen = document.getElementById('screen')
 
+// elemento e função dos botoes de numero
 const numbers = document.querySelectorAll('.button-number')
 numbers.forEach(number => {
-    number.addEventListener('click', () => {
+    number.addEventListener('click', () => {      
+        if(screen.textContent == 'Too big!' || screen.textContent == 'Invalid!') {
+            Allclear()
+        }
         if(screen.textContent.length < 12) {
             screen.textContent += number.textContent
+            if(!f) {
+                x = Number(screen.textContent)
+            } else {
+                y = Number(screen.textContent)
+            }
         }
     })
 })
 
+// limitação do ponto (.) unico no display
+dot.addEventListener('click', () => {
+    if(!(screen.textContent.includes('.'))) {
+        screen.textContent += dot.textContent
+    }
+})
+
+// elementos e botoes de operação
 const operations = document.querySelectorAll('.button-operation')
 operations.forEach(operation => {
     operation.addEventListener('click', () => {
         if(!f) {
-
             f = window[operation.getAttribute('id')]
-
+            screen.textContent = undefined
         } else {
-
+            x = operate(f, x, y)
+            f = window[operation.getAttribute('id')]
+            screen.textContent = undefined            
         }
     })
 })
 
+
+// botao de igual e calculo do resultado
+equal.addEventListener('click', setResult)
+function setResult() {
+    result = operate(f, x, y).toFixed(10)
+    if(result.toString().length > 12) {
+        Allclear()
+        screen.textContent = 'Too big!'
+    } else {
+        screen.textContent = result
+    }
+    if(result == Infinity) {
+        Allclear()
+        screen.textContent = 'Invalid!'
+        
+    }
+}
+
+// botao de limpeza
 ac.addEventListener('click', Allclear)
 function Allclear() {
     screen.textContent = undefined
     f = undefined
     x = undefined
     y = undefined
-}
-
-function clearFunction() {
-    screen.textContent = undefined
-    f = undefined
+    result = undefined
 }
